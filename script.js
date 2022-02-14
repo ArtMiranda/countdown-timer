4eddeee66af764ccedd8eb246ad0b5604c0493ad
Artur C. Miranda <artcamir12@gmail.com>
2022-02-14 17:51:28 -0300

    Initial commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var interval;
 function updateText(){
     let h = document.getElementsByTagName("h1")[0];
     h.innerHTML = number;
-    number++;
+    number--;
 }
 
 function start(){
@@ -26,4 +26,33 @@ function clearCount(){
     h.innerHTML = "0";
 
     clearInterval(interval);
+}
+
+
+document.getElementById("addTimeDiv").addEventListener("click", function(e){
+    const pressed = e.target;
+
+    addTime(pressed);
+
+});
+
+function addTime(pressed){
+
+    var addTimeDiv = document.getElementById("addTimeDiv");
+
+    if (pressed == addTimeDiv.firstChild){
+        console.log("5 minutes");
+    }
+    if (pressed.innerText == addTimeDiv.firstChild.firstChild){
+        console.log("1 minute");
+    }
+    if (pressed.innerText == addTimeDiv.firstChild.firstChild.firstChild){
+        console.log("30 seconds");
+    }
+    if (pressed.innerText == addTimeDiv.firstChild.firstChild.firstChild.firstChild){
+        console.log("1 second");
+    }
+    if (pressed.innerText == addTimeDiv.lastChild){
+        console.log("reset");
+    }
 }
